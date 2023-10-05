5886efaa32093f97a7093fa7d2d9c588a8547a94
Andrea Graf <andrea.graf@heig-vd.ch>
2023-10-05 11:56:01 +0200

    Add feature getEncoding()

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/AndreaGraf/EncodingSelector.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/AndreaGraf/EncodingSelector.java
@@ -17,7 +17,25 @@ public class EncodingSelector {
      * @return the encoding of the file, or null if the extension is not recognized
      */
     public Charset getEncoding(File file) {
-        // TODO: implement the method body here
+        if (file == null) {
+            return null;
+        }
+
+        String fileName = file.getName();
+
+        if (fileName.endsWith(".utf8"))
+            return Charset.forName("UTF-8");
+
+        if (fileName.endsWith(".txt"))
+            return Charset.forName("US-ASCII");
+
+        if (fileName.endsWith(".utf16be"))
+            return Charset.forName("UTF-16BE");
+
+        if (fileName.endsWith(".utf16le"))
+            return Charset.forName("UTF-16LE");
+
         return null;
+
     }
 }
