a3206fd5162d1c975f8aa089687a0c000645dd4f
JustinFerrara14 <justin.ferrara@heig-vd.ch>
2023-10-12 11:23:49 +0200

    FirstImplementMain

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
@@ -1,13 +1,13 @@
 package ch.heig.dai.lab.fileio;
 
 import java.io.File;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
-// *** TODO: Change this to import your own package ***
-import ch.heig.dai.lab.fileio.jehrensb.*;
+import ch.heig.dai.lab.fileio.AndreCostaaa.*;
 
 public class Main {
-    // *** TODO: Change this to your own name ***
-    private static final String newName = "Jean-Claude Van Damme";
+    private static final String newName = "Justin Ferrara";
 
     /**
      * Main method to transform files in a folder.
@@ -33,9 +33,31 @@ public class Main {
         System.out.println("Application started, reading folder " + folder + "...");
         // TODO: implement the main method here
 
+        FileExplorer fileExplorer = new FileExplorer(folder);
+        EncodingSelector encodingSelector = new EncodingSelector();
+        FileReaderWriter fileReaderWriter = new FileReaderWriter();
+        Transformer transformer = new Transformer(newName, wordsPerLine);
+
         while (true) {
             try {
-                // TODO: loop over all files
+
+                File file = fileExplorer.getNewFile();
+                if (file == null) {
+                    break;
+                }
+
+                Charset encoding = encodingSelector.getEncoding(file);
+                if(encoding == null) {
+                    continue;
+                }
+                String content = fileReaderWriter.readFile(file, encoding);
+                String result = transformer.replaceChuck(content);
+                result = transformer.capitalizeWords(result);
+                result = transformer.wrapAndNumberLines(result);
+
+                String resultFileName = file.getName() + ".processed";
+                File resultFile = new File(file.getParent(), resultFileName);
+                fileReaderWriter.writeFile(resultFile, result, StandardCharsets.UTF_8);
 
             } catch (Exception e) {
                 System.out.println("Exception: " + e);
