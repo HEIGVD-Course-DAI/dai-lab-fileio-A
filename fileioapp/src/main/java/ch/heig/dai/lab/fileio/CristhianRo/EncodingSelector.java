b7c604485af09ece06932342548a0ed15dcba3df
CristhianRo <114008453+CristhianRo@users.noreply.github.com>
2023-10-12 11:38:34 +0200

    Tranformer

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/CristhianRo/EncodingSelector.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/CristhianRo/EncodingSelector.java
@@ -1,9 +1,24 @@
 package ch.heig.dai.lab.fileio.CristhianRo;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
 
 public class EncodingSelector {
+
+
+    private static final Map<String, Charset> EXTENSION_TO_CHARSET = new HashMap<>();
+
+    static {
+        EXTENSION_TO_CHARSET.put("utf8", Charset.forName("UTF-8"));
+        EXTENSION_TO_CHARSET.put("txt", Charset.forName("US-ASCII"));
+        EXTENSION_TO_CHARSET.put("utf16be", Charset.forName("UTF-16BE"));
+        EXTENSION_TO_CHARSET.put("utf16le", Charset.forName("UTF-16LE"));
+    }
+
+
 
     /**
      * Get the encoding of a file based on its extension.
@@ -16,8 +31,20 @@ public class EncodingSelector {
      * @param file the file to get the encoding from
      * @return the encoding of the file, or null if the extension is not recognized
      */
-    public Charset getEncoding(File file) {
+    public Charset getEncoding(File file) { 
         // TODO: implement the method body here
-        return null;
+        if (file == null) {
+            return null;
+        }
+
+        String fileName = file.getName();
+        int extension = fileName.lastIndexOf('.');
+
+        if (extension == -1) {
+            return null;  // There's no extension
+        }
+
+        String encoding = fileName.substring(extension + 1).toLowerCase();
+        return EXTENSION_TO_CHARSET.get(encoding);
     }
 }
