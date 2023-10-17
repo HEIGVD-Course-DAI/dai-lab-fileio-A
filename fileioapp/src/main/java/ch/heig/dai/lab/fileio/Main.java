c96cd86383b1a3d6e985336c9e9076d5f0d3110e
GuilhermePintodac <guilherme.pintodacunhadamata@heig-vd.ch>
2023-10-17 22:11:31 +0200

    Main implemented

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
@@ -1,13 +1,14 @@
 package ch.heig.dai.lab.fileio;
 
 import java.io.File;
+import java.io.FileReader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
-// *** TODO: Change this to import your own package ***
-import ch.heig.dai.lab.fileio.jehrensb.*;
+import ch.heig.dai.lab.fileio.GuilhermePintodac.*;
 
 public class Main {
-    // *** TODO: Change this to your own name ***
-    private static final String newName = "Jean-Claude Van Damme";
+    private static final String newName = "Guilherme Pinto da Cunha da Mata";
 
     /**
      * Main method to transform files in a folder.
@@ -31,11 +32,35 @@ public class Main {
         String folder = args[0];
         int wordsPerLine = Integer.parseInt(args[1]);
         System.out.println("Application started, reading folder " + folder + "...");
-        // TODO: implement the main method here
 
+        FileExplorer fileExplorer = new FileExplorer(folder);
+        FileReaderWriter fileReaderWriter = new FileReaderWriter();
+        EncodingSelector encodingSelector = new EncodingSelector();
+        Transformer transformer = new Transformer(newName, wordsPerLine);
         while (true) {
             try {
-                // TODO: loop over all files
+
+                File inFile = fileExplorer.getNewFile();
+                if (inFile == null){
+                    break;
+                }
+                java.nio.charset.Charset encoding = encodingSelector.getEncoding(inFile);
+
+                if (encoding == null){
+                    continue;
+                }
+                String content = fileReaderWriter.readFile(inFile, encoding);
+
+
+                if (content == null){
+                    continue;
+                }
+                String transformedString = transformer.replaceChuck(content);
+                transformedString = transformer.capitalizeWords(transformedString);
+                transformedString = transformer.wrapAndNumberLines(transformedString);
+
+                File outFile = new File(inFile.getParent(), inFile.getName() + ".processed");
+                fileReaderWriter.writeFile(outFile, transformedString, StandardCharsets.UTF_8);
 
             } catch (Exception e) {
                 System.out.println("Exception: " + e);
