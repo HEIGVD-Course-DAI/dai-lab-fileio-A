49fce181b4fc20169d3ab4452d66281f734009cb
GuillaumeDnt2 <guillaume.dunant@heig-vd.ch>
2023-10-18 18:43:01 +0200

    Fin implémentation Main

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
@@ -1,13 +1,15 @@
 package ch.heig.dai.lab.fileio;
 
 import java.io.File;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 // *** TODO: Change this to import your own package ***
-import ch.heig.dai.lab.fileio.jehrensb.*;
+import ch.heig.dai.lab.fileio.GuillaumeDnt2.*;
 
 public class Main {
     // *** TODO: Change this to your own name ***
-    private static final String newName = "Jean-Claude Van Damme";
+    private static final String newName = "Guillaume Dunant";
 
     /**
      * Main method to transform files in a folder.
@@ -31,11 +33,38 @@ public class Main {
         String folder = args[0];
         int wordsPerLine = Integer.parseInt(args[1]);
         System.out.println("Application started, reading folder " + folder + "...");
+        
         // TODO: implement the main method here
+        FileExplorer fExplorer = new FileExplorer(folder);
+        EncodingSelector eSelector = new EncodingSelector();
+        FileReaderWriter fReaderWriter = new FileReaderWriter();
+        Transformer transformer = new Transformer(newName, wordsPerLine);
 
         while (true) {
             try {
-                // TODO: loop over all files
+                //Récupère le prochain fichier ou quitte le programme s'il n'y en a plus aucun
+                File file = fExplorer.getNewFile();
+                if(file == null){
+                    System.out.println("Plus aucun fichier à lire!");
+                    System.exit(0);
+                }
+
+                //Récupère le charset et passe le fichier s'il n'est pas reconnu
+                Charset charset = eSelector.getEncoding(file);
+                if(charset == null){
+                    continue;
+                }
+
+                //Lit le contenu et le transforme
+                String content = fReaderWriter.readFile(file, charset);
+                StringBuilder sb = new StringBuilder(transformer.replaceChuck(content));
+                sb.append("\n" + transformer.capitalizeWords(content));
+                sb.append("\n" + transformer.wrapAndNumberLines(content));
+
+                //Crée le nouveau fichier et écrit le contenu
+                File newFile = new File(file.getAbsolutePath() + ".processed");
+                fReaderWriter.writeFile(newFile, sb.toString(), StandardCharsets.UTF_8);
+        
 
             } catch (Exception e) {
                 System.out.println("Exception: " + e);
