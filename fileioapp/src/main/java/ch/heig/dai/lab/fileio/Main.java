2f5752b54bb90b3a69ee5f50ae7b97c7890df1c2
G-LK <114987519+G-LK@users.noreply.github.com>
2023-10-18 14:59:39 +0200

    End of main

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
@@ -1,13 +1,19 @@
 package ch.heig.dai.lab.fileio;
 
 import java.io.File;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 // *** TODO: Change this to import your own package ***
-import ch.heig.dai.lab.fileio.jehrensb.*;
+import ch.heig.dai.lab.fileio.GLK.EncodingSelector;
+import ch.heig.dai.lab.fileio.GLK.FileExplorer;
+import ch.heig.dai.lab.fileio.GLK.FileReaderWriter;
+import ch.heig.dai.lab.fileio.GLK.Transformer;
+import ch.heig.dai.lab.fileio.GLK.*;
 
 public class Main {
     // *** TODO: Change this to your own name ***
-    private static final String newName = "Jean-Claude Van Damme";
+    private static final String newName = "Leonard";
 
     /**
      * Main method to transform files in a folder.
@@ -15,7 +21,7 @@ public class Main {
      * In an infinite loop, get a new file from the FileExplorer, determine its encoding with the EncodingSelector,
      * read the file with the FileReaderWriter, transform the content with the Transformer, write the result with the
      * FileReaderWriter.
-     * 
+     *
      * Result files are written in the same folder as the input files, and encoded with UTF8.
      *
      * File name of the result file:
@@ -29,13 +35,52 @@ public class Main {
             System.exit(1);
         }
         String folder = args[0];
+
+        //permet de connaître le nombre de mots par ligne.
         int wordsPerLine = Integer.parseInt(args[1]);
         System.out.println("Application started, reading folder " + folder + "...");
         // TODO: implement the main method here
 
+        FileExplorer fileExplorer = new FileExplorer(folder);
+        FileReaderWriter fileReaderWriter = new FileReaderWriter();
+        EncodingSelector encodingSelector = new EncodingSelector();
+        Transformer transformer = new Transformer(newName, wordsPerLine);
+
+
         while (true) {
             try {
                 // TODO: loop over all files
+                File file = fileExplorer.getNewFile();
+                if(file == null) {
+                    System.out.println("no files found");
+                    break;
+                }
+
+
+                Charset encoding = encodingSelector.getEncoding(file);
+                if(encoding == null) {
+                    System.out.println("not the right encoding");
+                    continue;
+                }
+
+                String content = fileReaderWriter.readFile(file, encoding);
+                if(content == null) {
+                    System.out.println("cannot read file");
+                    continue;
+                }
+
+                content = transformer.capitalizeWords(content);
+                content = transformer.replaceChuck(content);
+                content = transformer.wrapAndNumberLines(content);
+
+                File result = new File(file + ".processed");
+                boolean hasWritten = fileReaderWriter.writeFile(result, content, encoding);
+                if(hasWritten){
+                    System.out.println(result.getName() + "file was not written");
+                } else {
+                    System.out.println(result.getName() + "file was written correctly");
+                }
+
 
             } catch (Exception e) {
                 System.out.println("Exception: " + e);
